c9128f13e8abb8edb756a45a18f90962a70ad727
BINAINU SHAIBU <149484056+BINAINUSHAIBU@users.noreply.github.com>
2026-09-24 23:15:27 +0100

    Delete js directory

--- js/admin.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('uploadForm').addEventListener('submit', function(e){
-e.preventDefault();
-document.getElementById('status').innerText = 'Video uploaded successfully!';
-});
